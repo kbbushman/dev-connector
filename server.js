165ce7b69e3ed6282009119b030b076764e19c25
kbbushman <k.bushman@yahoo.com>
2018-06-05 13:35:45 -0700

    Adds new user registration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Server Port
 const port = process.env.PORT || 5000;
@@ -10,6 +11,10 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
