833f3160062b0e10a874d0f9d657fe6436b56934
kbbushman <k.bushman@yahoo.com>
2019-03-24 14:16:19 -0700

    Adds post feed route and view

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import {
   ADD_POST,
   GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
 }  from './types';
 
 // Add Post
@@ -20,3 +22,28 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios.get('/api/posts')
+    .then(res => 
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })  
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set Loading State
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
+}
