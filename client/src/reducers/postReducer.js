f811bfc4192692f4da61d95245af4cbc675595ca
kbbushman <k.bushman@yahoo.com>
2019-03-24 16:29:43 -0700

    Adds single post route and view

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,6 +1,7 @@
 import {
   ADD_POST,
   GET_POSTS,
+  GET_POST,
   POST_LOADING,
   DELETE_POST,
 } from '../actions/types';
@@ -24,6 +25,12 @@ const postReducer = (state = initialState, action) => {
         posts: action.payload,
         loading: false,
       };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
     case ADD_POST:
       return {
         ...state,
@@ -33,7 +40,7 @@ const postReducer = (state = initialState, action) => {
       return {
         ...state,
         posts: state.posts.filter(post => post._id !== action.payload)
-      }
+      };
     default:
       return state;
   }
