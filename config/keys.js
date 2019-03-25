0582ac2f135f40ccc8e1a9e9f39971d4fbb1b8eb
kbbushman <k.bushman@yahoo.com>
2019-03-24 17:17:34 -0700

    Adds deployment config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://kennyb:purplpurpl22@ds011933.mlab.com:11933/devcnctr-dev',
-  secretOrKey: 'secretsecret',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
