da791e624681a0f64ceb807fb147dd198fa9bceb
kbbushman <k.bushman@yahoo.com>
2019-03-24 17:04:28 -0700

    Adds comments to single post view

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
-import { addPost } from '../../actions/postActions';
+import { addComment } from '../../actions/postActions';
 
-class PostForm extends Component {
+class CommentForm extends Component {
   state = {
     text: '',
     errors: {},
@@ -26,14 +26,15 @@ class PostForm extends Component {
     e.preventDefault();
     
     const { user } = this.props.auth;
+    const { postId } = this.props;
 
-    const newPost = {
+    const newComment = {
       text: this.state.text,
       name: user.name,
       avatar: user.avatar,
     }
 
-    this.props.addPost(newPost);
+    this.props.addComment(postId, newComment);
     this.setState({ text: '' });
   }
 
@@ -43,14 +44,12 @@ class PostForm extends Component {
     return (
       <div className="post-form mb-3">
         <div className="card card-info">
-          <div className="card-header bg-info text-white">
-            Say Somthing...
-          </div>
+          <div className="card-header bg-info text-white">Make a comment...</div>
           <div className="card-body">
             <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <TextAreaFieldGroup
-                  placeholder="Create a post"
+                  placeholder="Reply to post"
                   name="text"
                   value={this.state.text}
                   onChange={this.handleChange}
@@ -66,9 +65,10 @@ class PostForm extends Component {
   }
 }
 
-PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired,
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
   errors: PropTypes.object.isRequired,
 }
 
@@ -77,4 +77,4 @@ const mapStatetoProps = state => ({
   errors: state.errors,
 })
 
-export default connect(mapStatetoProps, { addPost })(PostForm);
+export default connect(mapStatetoProps, { addComment })(CommentForm);
